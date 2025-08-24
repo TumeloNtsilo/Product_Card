51cf6c51f45935116dd1a7614e43c2faa9ab99e8
Tumelo <tumelontsilo@gmail.com>
2025-08-24 21:23:55 +0200

    Created the html file and added the image and background color

---
+++ config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend : {
+            colors : {
+                pale_orange : '#ffd9a6',   //I can use this to override ny color on my html
+                light_orange : '#fbb03b',
+                orange : '#f7931e',
+            },
+
+            fontFamily : {
+                sans : ['Bebas Neue', 'sans-serif'],
+                slab : ['Robot Slab', 'serif']
+            }
+            
+        }
+    }
+}
+
